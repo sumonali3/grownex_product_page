41ee9e7da0321e8e125fa44380e6e2e0c4dcb038
ali <admin@sumonify.com>
2025-06-10 09:52:34 +0600

    update

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    // Your content paths here, e.g.,
+    "./index.html",
+    "./src//*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      colors: {
+        'brand-teal': '#14B8A6',
+        'brand-teal-dark': '#0D9488',
+        'brand-dark': '#1F2937',
+        'brand-text-medium': '#4B5563',
+        'brand-text-light': '#6B7281',
+        'brand-bg-light': '#F9FAFB',
+        'brand-bg-white': '#FFFFFF',
+        'brand-border': '#D1D5DB',
+      }
+    }
+  },
+  plugins: [],
+}
